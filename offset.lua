879a1bad4716ff6f74de5d6520e23017a719818e
n0thhhing <lynchlm064@gmail.com>
2023-12-17 16:11:13 -0500

    wat

---
+++ offset.lua
@@ -0,0 +1,81 @@
+0x491F3B4 --clan parts 23.8.0
+0x491FEA0 --clan parts second offset 23.8.0
+0x491F0AC --clan energy 23.8.0
+0x17C9190 --free lottery 23.8.0
+0x2BD4F20 --&lottocurrency 23.8.0
+0x320F340 --&Collectibles 23.8.0
+0x17C9190 --&freechestcount 23.8.0
+0x2240E5C --gem clicker 23.8.0
+0x3ED0EC8 --&taskClicker 23.8.0
+0x215CBEC --&pixelClicker 23.8.0
+0x1E19050 --&blackMarketClicker 23.8.0
+0x3393944 --&gadgetUnlocker 23.8.0
+0x3216E34 --&%fmodule 23.8.0
+0x3954EBC --&MaxModules 23.8.0
+0x3955750 --&MaxModules 23.8.0
+0x3216C84 --&MaxModules 23.8.0
+0x32173EC --&NoPartsModules 23.8.0
+0x282A3CC --&noGadgetCooldown 23.8.0
+0x282A448 --&noGadgetCooldown 23.8.0
+0x20914B4 --&xpl 23.8.0 {search
+0x49665D0 --&lobbybundles 23.8.0
+0x3EEB14C --&validatenickname2[both set to true] 23.8.0
+0x3EEB38C --&validatenickname1 23.8.0
+0x5464D40 --&fire rate 23.8.0
+0x26C5FF4 --&infammo 23.8.0
+0x1915F9C --&teamkill[1] [both set to false] 23.8.0
+0x1917360 --&teamkill[2] 23.8.0
+0x2321090 --&swearfilter[set to false] 23.8.0
+0x551EED8 --&god[1] 23.8.0 [all set to -> C0035FD6]
+0x2862DBC --&god[2] 23.8.0
+0x52AA7E0 --&god[3] 23.8.0
+0x52A9F50 --&god[4] 23.8.0
+0x2602920 --&turrentgod[1] 23.8.0 [all set to C0035FD6]
+0x2607A78 --&turrentgod[2] 23.8.0 done
+0x59816AC --&turrentgod[3] 23.8.0 done
+0x48CE438 --&petrespawn 23.8.0
+0x3A0A900 --&arenascore 23.8.0
+0x597E240 --&GetFreeCheckpointsCount 23.8.0
+0x17CB81C --&rarity 23.8.0
+0x3D36598 --useless not needed
+0x3D364F8 --useless not needed
+0x419B848 --Rewards Multiplier In a Game (99x MAX)
+0x255F250 --Unlimited Super Clan Chest,
+0x255F998 --Clan Chest Open Price[1]
+0x2560730 --Clan Chest Open Price[2]
+0x2560730 --Super Clan Chest Points Modifier
+0x41A2218 --Egg Reward Modificator[1]
+0x41A2218 --Egg Reward Modificator[2] its 9 up from the previous offsets class with an item rarity above it done
+0x32F5130 --[1]Give all wears/armors/graffiti require parts
+0x32F4EF8 --[2]10 above this^^, make sure the argument has an int in it, ex: internal int丕丄且世丏丄不下 (int 万丆万上丌上丒专上) { } done
+0x5987C7C --campaign
+0x547EB1C --guns/shovel 2 done
+0x3219554 --guns/shovel 2 done
+0x547EB1C --royal
+0x43AB748 --royal
+0x43B20C0 --wep skins
+0x4549EC0 --wep skins
+0x439FA64 --just armor
+0x20913F4 --set custom level
+0x52AE22C --Show all Deleted/Exclusive Gadgets[1] 23.8.0 edit both to true
+0x384E100 --Show all Deleted/Exclusive Gadgets[2]
+0x2E8C8D8 --emperor 23.8.0
+0x419B784 --inf 2x rew
+0x2150EA0 --max pass[temp]
+0x2157938 --premium pass
+0x2153C28 --reset pass
+0x41A5E98 --pet egg cd
+0x5B6E838 --mobs per wave
+0x3393944 --clan gadgets
+0x229520C --set clicker done
+0x52ADABC --unlock unreleased royal and hats
+0x4549EC0 --wear parts [401F80D2C0035FD6 ]search " { get; } [new line] internal static Dictionary<string, Dictionary<string," with a list string above it and go 10 down from methods done
+0x3201B88 --wear [true]
+0x215D3DC --gift pass offers 1
+0x215D450 --gift pass offers 2
+0x215D77C --gift pass offers 3
+0x5617EC0 --clan wear
+0x24EC370 --collectibles v2 [true] combined with vd3  search "internal DateTime StartBuyProxyDate" and go 3 classes down and the bool is 20 up with an int below it
+0x439FA64 --unreleased royal [false] done 
+0x52AE91C -- unreleased royal [true] done
+0x17C9190 --armory price[not weps] done
